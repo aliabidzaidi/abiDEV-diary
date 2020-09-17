bcecfb46fd5f9149fda0224eab55a36b1754e72b
Abid Zaidi <abidzaidi7@hotmail.com>
2020-09-17 23:50:30 +0500

    Adding initial code from template

---
+++ src/components/header/MobileBio.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import "./header.css"
+// import willjw3 from "../../images/willjw3.jpg"
+import abidzaidi from "../../images/cartoon-x.png"
+
+const MobileBio = (props) => {
+
+    return (
+        <div className="mobile-bio-main">
+            <img src={abidzaidi}  className="ml-4 mt-2" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
+            <h4 className="mr-4 mt-4">{props.author}</h4>
+        </div>
+    )
+}
+
+export default MobileBio
---
+++ src/components/sidebar/Bio.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./sidebar.css"
+
+import abidzaidi from "../../images/cartoon-x.png"
+// import willjw3 from "../../images/willjw3.jpg"
+
+const Bio = ({ author, tagline }) => {
+
+    return (
+        <div className="bio-main w-75">
+            <img src={abidzaidi} style={{ maxWidth: `100px` }} className="profile-img" alt="" />
+            <h3 className="mt-2 author-bio">{author}</h3>
+            <small className="text-muted">{tagline}</small>
+        </div>
+    )
+}
+
+export default Bio
